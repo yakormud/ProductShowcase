7f1beb1046f22169d15a03b1aa4d8ca55f1b4dc1
YaKoRMuD <iamyakormud@gmail.com>
2024-05-04 21:52:15 +0700

    add server.js file

---
+++ sqlconfig.cjs
@@ -0,0 +1,12 @@
+const sql = require("mssql");
+
+const config = {
+  server: 'LAPTOP-E4MO904H\SQLEXPRESS',
+  database: 'Northwind',
+  user: 'sa',
+  password: '123456789',
+  encrypt: false,
+  trustServerCertificate: false,
+};
+
+module.exports = sql;
--- sqlconfig.js
+++
@@ -1,9 +0,0 @@
-const sql = require("mssql");
-const config = {
-server: 'LAPTOP-FLA3D2A9\\SQLEXPRESS',
-database: 'Northwind',
-user: 'sa',
-password: 'P@ssword',
-encrypt: false,
-trustServerCertificate: false,
-};
