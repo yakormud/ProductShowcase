492fc1c0a563a77c7709659f24060816b4d43e30
YaKoRMuD <iamyakormud@gmail.com>
2024-05-04 22:19:46 +0700

    Update sqlconfig.cjs

--- sqlconfig.cjs
+++ sqlconfig.cjs
@@ -1,7 +1,7 @@
 const sql = require("mssql");
 
 const config = {
-  server: 'LAPTOP-E4MO904H\SQLEXPRESS',
+  server: 'LAPTOP-E4MO904H\\SQLEXPRESS',
   database: 'Northwind',
   user: 'sa',
   password: '123456789',
