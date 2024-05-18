48e5e4c0dc767bd93837190b386d6402e2ec266b
Narinphat Limpavittayapon <exserruim@gmail.com>
2024-05-18 13:34:49 +0700

    adding Authentication(not finish)

--- server.js
+++ server.js
@@ -6,10 +6,9 @@ import multer from 'multer';
 import fs from 'fs';
 import sql from "mssql";
 import path from 'path';
-
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
-
+import createStore from 'react-auth-kit/createStore';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
@@ -18,28 +17,59 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(express.static('public'));
 
-// const config = {
-//   server: 'productdb.czw620y8qg76.us-east-1.rds.amazonaws.com',
-//   database: 'Product',
-//   user: 'adminlogin',
-//   password: '123456789',
-//   encrypt: false,
-//   trustServerCertificate: false,
-// };
+
+const store = createStore({
+  authName:'_auth',
+  authType:'cookie',
+  cookieDomain: window.location.hostname,
+  cookieSecure: window.location.protocol === 'https:',
+});
+
 
 async function testConnection() {
   try {
     await sql.connect(config);
     console.log('Connected to the database successfully!');
   } catch (error) {
-    // Log any errors that occur during connection attempt
     console.error('Error connecting to the database:', error.message);
   } finally {
-    // Close the connection pool to free up resources
     await sql.close();
   }
 }
 testConnection();
+
+// async function createTestUser(){
+//   try {
+//     await sql.connect(config).then(pool =>{
+//       return pool.request().query('INSERT INTO user (UserName,UserPassword,FirstName,LastName,PathToPicture) Values (admin1,abc12345,Narinphat,Limpavittayapon,)');
+//     })
+//   } catch (error) {
+//     console.error('Error connecting to the database:', error.message);
+//   } finally {
+//     await sql.close();
+//   }
+// }
+
+//get auth
+app.post('/auth',(req,res) =>{
+
+  const {username , password} = req.body;
+  
+  sql.connect(config)
+    .then(pool => {
+      return pool.request().input('UserName', sql.NVarChar, username).query('SELECT * FROM user WHERE userName=@UserName');
+    })
+    .then(result => {
+      res.json(result.recordset);
+    })
+    .catch(err => {
+      console.error('Error:', err);
+      res.status(500).json({ error: 'Internal Server Error' });
+    });
+
+
+})
+
 
 // get all products
 
--- src/Login.jsx
+++ src/Login.jsx
@@ -10,6 +10,8 @@ import axios from 'axios';
 
 const Login = () => {
     const [user, setUsers] = useState([]);
+
+    
     useEffect(() => {
         axios.get(`http://localhost:80/login`, {
         }).then((res) => res.data).then(data => {
@@ -24,7 +26,7 @@ const Login = () => {
         password: "",
         confirmPassword: "",
       });
-    //After the form state is defined, we have the onUpdateField function, which is passed to each input field as an onChange handler. Even though we have three form fields, we don’t need separate handlers for them. We can use just one function by utilizing field’s name attribute as a form key.
+    
       const onUpdateField = e => {
         const nextFormState = {
           ...form,
@@ -32,7 +34,7 @@ const Login = () => {
         };
         setForm(nextFormState);
       };
-      //Further, the onSubmitForm method will be executed when the form is submitted. At the moment, it just prevents the default form submit behavior and then shows an alert with the form’s values.
+      
       const onSubmitForm = e => {
         e.preventDefault();
         alert(JSON.stringify(form, null, 2));
