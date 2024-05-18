56f47ae4f74a8a7ddd15ddb170292137c45b6b7c
YaKoRMuD <iamyakormud@gmail.com>
2024-05-18 21:24:44 +0700

    Update Product.jsx

--- src/Product.jsx
+++ src/Product.jsx
@@ -3,8 +3,19 @@ import logo from './assets/mylogo.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 function Product() {
+    const [test, setTest] = useState([]);
+
+  useEffect(() => {
+    axios.get(`http://localhost:80/product`, {
+    }).then((res) => res.data).then(data => {
+      setTest(data);
+    }).catch((error) => {
+      console.log(error);
+    });
+
+  }, []);
     return (
-        <h3>Product</h3>
+        <h3>{test.productID}</h3>
     )
 }
 
